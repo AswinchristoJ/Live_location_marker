9f11fdc44d87cfa1481ab6ca9104f907a8f65913
AswinChristo <aswinj2019@srishakthi.ac.in>
2018-12-07 11:25:43 +0530

    first upload

---
+++ public/js/script.js
@@ -0,0 +1,54 @@
+$(document).ready(() => {
+
+    let socket = io.connect()
+
+    let userName;
+
+    swal("Enter your name here:", {
+        content: "input",
+    })
+        .then((value) => {
+            userName = value
+
+            if (value == '') {
+                userName = 'Anonymous'
+            }
+
+            navigator.geolocation.getCurrentPosition(function (location) {
+
+                let data = {
+                    'coords': location.coords.latitude + ',' + location.coords.longitude,
+                    'userName': userName
+                }
+
+                socket.emit('coordinates', data)
+            });
+        });
+
+    socket.on('userStatus', (message) => {
+        console.log(message)
+    })
+
+    socket.on('broad', (data) => {
+
+        let value;
+        let count;
+        if (Object.keys(data).length != 0) {
+
+            value = ''
+            count = 0
+            for (let key in data) {
+                count++
+                value = value + `${data[key][1]}|flag-lg-4DBD33-${data[key][0]}`
+                if (count != Object.keys(data).length) {
+                    value = value + '||'
+                }
+            }
+
+            document.getElementsByTagName('iframe')[0].src = `https://www.mapquestapi.com/staticmap/v5/map?key=7qAHtUkSymItMvSGgAl9Ib1ji8wc6lkV&scalebar=true|bottom&banner=locations+of+those+online|sm-top&locations=${value}&zoom=12&size=1500,800`
+            console.log(document.getElementsByTagName('iframe')[0].src)
+        }
+
+    })
+
+})
---
+++ src/app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const socket = require('socket.io')
+
+let port = process.env.PORT || 3200
+
+let server = app.listen(port, () => {
+    console.log(`app listening on ${port}`)
+})
+
+let io = socket(server)
+
+let coords = {}
+
+io.on('connection', (socket) => {
+    socket.broadcast.emit('userStatus', 'a new user is connected')
+
+    socket.on('coordinates', (data) => {
+
+        coords[socket.id] = []
+        coords[socket.id].push(data.userName)
+        coords[socket.id].push(data.coords)
+        io.emit('broad', coords)
+    })
+
+    socket.emit('broad', coords)
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('userStatus', 'someone is disconnected')
+        delete coords[socket.id]
+        console.log(coords)
+        io.emit('broad', coords)
+
+    })
+
+})
+
+app.use(express.static(__dirname + '/../public/'))
+
+
+
+
